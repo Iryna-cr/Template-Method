df6ff1e752f272df5ac3f17b289fb1530e032df7
Iryna <kytsenkoiryna33@gmail.com>
2025-11-03 00:22:13 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class OrderProcess
+{
+    public void Process()
+    {
+        SelectFlowers();
+        Pay();
+        Deliver();
+    }
+
+    protected void SelectFlowers() => Console.WriteLine("Вибір квітів");
+    protected void Pay() => Console.WriteLine("Оплата замовлення");
+    protected void Deliver() => Console.WriteLine("Доставка оформлена");
+}
+
+class Program
+{
+    static void Main()
+    {
+        new OrderProcess().Process();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
